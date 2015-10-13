8df5b5cadaad44b54b8ff2c459037a26fb6f7ea7
Zain Rizvi <ZainRizvi@users.noreply.github.com>
2015-10-12 23:39:43 -0700

    Adding default 'yo angular' sample project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularOnAzureWebAppsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoAngularOnAzureWebAppsApp
+ */
+angular.module('yoAngularOnAzureWebAppsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularOnAzureWebAppsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoAngularOnAzureWebAppsApp
+ */
+angular.module('yoAngularOnAzureWebAppsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
